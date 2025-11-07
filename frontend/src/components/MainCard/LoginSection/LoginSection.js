0895910c3d30ca227ae96df98382bd2e2296d9d7
Plamen <plamenkosharov777@gmail.com>
2025-11-07 18:21:02 +0200

    feat(LoginSection): add form state handling and submit functionality

--- frontend/src/components/MainCard/LoginSection/LoginSection.js
+++ frontend/src/components/MainCard/LoginSection/LoginSection.js
@@ -1,8 +1,26 @@
 import styles from "./LoginSection.module.css"
+import {useState} from "react";
 
 export default function LoginSection(){
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    rememberMe: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value, // ako ne e checkbox, setni stoinosta na inputa, ako e checkbox, setni checked property-to
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log("Form data:", formData);
+
+    // tuka shte bude fetcha kogato backenda e gotov
   };
 
   return(
@@ -11,17 +29,37 @@ export default function LoginSection(){
       <form className={styles.form}>
         <div className={styles.formContainer}>
           <div className={styles.inputGroup}>
-            <input type="text" id="username" name="username" required placeholder=" " />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder=""
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
             <label htmlFor="username">Потребителско Име</label>
           </div>
 
           <div className={styles.inputGroup}>
-            <input type="password" id="password" name="password" required placeholder=" " />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder=" "
+              required/>
             <label htmlFor="password">Парола</label>
           </div>
 
-          <label style={{ display: "flex", alignItems: "center", gap: "6px", fontSize: "0.9rem", color: "#555" }}>
-            <input type="checkbox" />
+          <label className={styles.checkBox}>
+            <input
+              type="checkbox"
+              name="rememberMe"
+              checked={formData.rememberMe}
+              onChange={handleChange}
+            />
             <span>Remember Me</span>
           </label>
 
