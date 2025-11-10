c5f3137999e573a6a03a9190a33d162e9ebac0ad
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-11-10 18:11:59 +0200

    add authentication routes to express app

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import userController from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.post("/logout", authMiddleware, userController.logout);
+
+export default router;
