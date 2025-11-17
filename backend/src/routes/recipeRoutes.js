d3af9c7779f790819cf1c33daa6f43ae616ff1f3
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-11-17 18:39:43 +0200

    update route paths for edit and delete

--- backend/src/routes/recipeRoutes.js
+++ backend/src/routes/recipeRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/", authMiddleware, recipeController.createRecipe);
 router.get("/group/:food_group_id", authMiddleware, recipeController.getRecipesByFoodGroup);
-router.put("/:recipeId", authMiddleware, recipeController.updateRecipe);
-router.delete("/:recipeId", authMiddleware, recipeController.deleteRecipe);
+router.put("/edit/:recipeId", authMiddleware, recipeController.updateRecipe);
+router.delete("/delete/:recipeId", authMiddleware, recipeController.deleteRecipe);
 
 export default router;
