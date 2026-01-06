413c2d03f41d3f0ed5b9c233a3362df2437c89d7
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2026-01-06 10:37:43 +0200

    add API and auth service modules

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,16 @@
+import api from "./api";
+import { clearUserData } from "../utils/storage";
+
+export const login = async (username, password) => {
+    const { data } = await api.post("/auth/login", { username, password });
+    return data;
+};
+
+export const register = async (formData) => {
+    const { data } = await api.post("/auth/register", formData);
+    return data;
+};
+
+export const logout = async () => {
+    clearUserData();
+};
