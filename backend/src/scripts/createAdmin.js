3c4f4c03c0959c9cbce2e8f53ee33d6e9957db5e
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2026-01-09 10:29:17 +0200

    add script to create admin user

---
+++ backend/src/scripts/createAdmin.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+await mongoose.connect(process.env.MONGO_URI);
+
+const password = await bcrypt.hash("Admin654123", 10);
+
+await User.create({
+    username: "admin",
+    email: "admin@easyhaccp.bg",
+    role: "admin",
+    password_hash: password,
+    active: true
+});
+
+console.log("Admin created");
+process.exit();
