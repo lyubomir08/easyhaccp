9da918a9183449e427aabf6393c6ec09f6b816cf
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2026-05-02 14:23:00 +0300

    Create .eslintrc.cjs

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  rules: {
+    'no-unused-vars': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/immutability': 'off',
+    'react-hooks/set-state-in-effect': 'off',
+    'react-hooks/static-components': 'off',
+    'react-refresh/only-export-components': 'off',
+    'no-empty': 'warn'
+  }
+}
