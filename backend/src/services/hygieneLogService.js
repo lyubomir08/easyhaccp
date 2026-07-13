81c47662a18fd2a18327f4dda0c0f43bba8f291c
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2026-07-13 17:11:36 +0300

    Update hygieneLogService.js

--- backend/src/services/hygieneLogService.js
+++ backend/src/services/hygieneLogService.js
@@ -1,81 +1,6 @@
 import HygieneLog from "../models/logs/HygieneLog.js";
 import ObjectModel from "../models/Object.js";
 import buildDateFilter from "../utils/buildDateFilter.js";
-import Room from "../models/Room.js";
-import Disinfectant from "../models/Disinfectant.js";
-import Employee from "../models/Employee.js";
-
-const parseWorkingHours = (working_hours) => {
-    if (!working_hours) return { closeH: 20, closeM: 0 };
-
-    const match = working_hours.match(
-        /(\d{1,2}):(\d{2})\s*[-–]\s*(\d{1,2}):(\d{2})/
-    );
-
-    if (!match) return { closeH: 20, closeM: 0 };
-
-    return {
-        closeH: Number(match[3]),
-        closeM: Number(match[4]),
-    };
-};
-
-const autoGenerateTodayLogs = async (object_id) => {
-    const now = new Date();
-
-    const todayStart = new Date(now);
-    todayStart.setHours(0, 0, 0, 0);
-
-    const todayEnd = new Date(now);
-    todayEnd.setHours(23, 59, 59, 999);
-
-    const object = await ObjectModel.findById(object_id);
-    if (!object) return;
-
-    const { closeH, closeM } = parseWorkingHours(object.working_hours);
-
-    const logTime = new Date(now);
-    logTime.setHours(closeH - 1, closeM, 0, 0);
-
-    if (now < logTime) return;
-
-    const rooms = await Room.find({ object_id });
-    if (!rooms.length) return;
-
-    const disinfectants = await Disinfectant.find({ object_id });
-    if (!disinfectants.length) return;
-
-    const employees = await Employee.find({ object_id });
-    if (!employees.length) return;
-
-    for (const room of rooms) {
-        const exists = await HygieneLog.findOne({
-            object_id,
-            room_id: room._id,
-            date: {
-                $gte: todayStart,
-                $lte: todayEnd
-            }
-        });
-
-        if (exists) continue;
-
-        const randomEmployee =
-            employees[Math.floor(Math.random() * employees.length)];
-
-        const randomDisinfectant =
-            disinfectants[Math.floor(Math.random() * disinfectants.length)];
-
-        await HygieneLog.create({
-            object_id,
-            date: logTime,
-            room_id: room._id,
-            disinfectant_id: randomDisinfectant._id,
-            employee_id: randomEmployee._id
-        });
-        console.log(`[HygieneCron] Автоматичен запис: ${object.name} - ${room.name}`);
-    }
-};
 
 const createHygieneLog = async (data) => {
     const { object_id } = data;
@@ -87,7 +12,6 @@ const createHygieneLog = async (data) => {
 };
 
 const getHygieneLogs = async (object_id, queryParams) => {
-    await autoGenerateTodayLogs(object_id);
     const page = Number(queryParams.page) || 1;
     const limit = Number(queryParams.limit) || 10;
     const skip = (page - 1) * limit;
